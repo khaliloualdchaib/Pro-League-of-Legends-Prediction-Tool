e9dcb673efe8ec72f2542e11b06f7659f9653329
khaliloualdchaib <oualdchaibkhalil@gmail.com>
2024-04-26 20:38:29 +0200

    champ select finished

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import "./App.css";
 import map from "./img/SRFull.jpg";
-import TeamSelector from "./componenets/teamSelector";
+import ChampSelect from "./componenets/champSelect";
 function App() {
   return (
     <div className="App">
       <h1>League of Legends Prediction Tool</h1>
-      <TeamSelector></TeamSelector>
-      <div class="flex flex-row">
-        <div class="basis-1/5">01</div>
-        <div class="basis-3/5">
-          <img src={map} alt="" />
+      <div className="flex flex-row">
+        <div className="basis-1/5">
+          <ChampSelect team="blue"/>
         </div>
-        <div class="basis-1/5">03</div>
+        <div className="basis-3/5 px-4 map-container">
+          <img src={map} alt="Map" className="map-image" />
+        </div>
+
+        <div className="basis-1/5">
+          <ChampSelect team="red"/>
+        </div>
       </div>
     </div>
   );
