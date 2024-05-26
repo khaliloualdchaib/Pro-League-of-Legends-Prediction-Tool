b037ba591235fea9e08cc28d0f3b57de743f6b46
Ferit <ferit.murad@student.uantwerpen.be>
2024-05-26 02:58:37 +0200

    testing

--- api/test.py
+++ api/test.py
@@ -2,20 +2,18 @@ from src.utils import get_column_values_from_csv, get_players, calculate_player_
 
 
 
-#print(get_most_kills("Jojopyun"))
-#print(get_column_values_from_csv(3,True))
-#print(get_players("G2 Esports"))
-#print(counter_pick("Sion","Gwen"))
-#print(calculate_player_stats(player_name="Faker",champion="Ahri"))
+print(get_column_values_from_csv(3,True))
+print(get_players("G2 Esports"))
+print(calculate_player_stats(player_name="Faker",champion="Ahri"))
 
-#team2Players = ["Zeus","Oner","Faker","Gumayusi","Keria"]
-#team2Champs = ["K'Sante","Viego","Taliyah","Varus","Nautilius"]
-#team1Players = ["Bin","Xun","knight","Elk","ON"]
-#team1Champs = ["Camille","Xin Zhao","Neeko","Senna","Orn"]
+team2Players = ["Zeus","Oner","Faker","Gumayusi","Keria"]
+team2Champs = ["K'Sante","Viego","Taliyah","Varus","Nautilius"]
+team1Players = ["Bin","Xun","knight","Elk","ON"]
+team1Champs = ["Camille","Xin Zhao","Neeko","Senna","Orn"]
 
-#print(get_predictions(team1Players=team1Players,team1Champs=team1Champs,team2Players=team2Players,team2Champs=team2Champs,prediction_target="result"))
-#print(get_predictions(team1Players=team1Players,team1Champs=team1Champs,team2Players=team2Players,team2Champs=team2Champs,prediction_target="firsttower"))
-#print(get_predictions(team1Players=team1Players,team1Champs=team1Champs,team2Players=team2Players,team2Champs=team2Champs,prediction_target="firstbaron"))
+print(get_predictions(team1Players=team1Players,team1Champs=team1Champs,team2Players=team2Players,team2Champs=team2Champs,prediction_target="result"))
+print(get_predictions(team1Players=team1Players,team1Champs=team1Champs,team2Players=team2Players,team2Champs=team2Champs,prediction_target="firsttower"))
+print(get_predictions(team1Players=team1Players,team1Champs=team1Champs,team2Players=team2Players,team2Champs=team2Champs,prediction_target="firstbaron"))
 
 
 print(player_only_statistics("Impact")[0])
